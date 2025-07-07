220a8f1d85b15d2c430e984c509b786df24f258e
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-07-07 11:53:52 +0600

    Изменил shouldSaveMultipleTasks(), добавлены новые таски

--- test/manager/tasks/FileBackedTaskManagerTest.java
+++ test/manager/tasks/FileBackedTaskManagerTest.java
@@ -23,21 +23,21 @@ public class FileBackedTaskManagerTest {
         assertTrue(loadedManager.getAllTask(Epic.class).isEmpty(), "Список эпиков должен быть пустым");
         assertTrue(loadedManager.getAllTask(Subtask.class).isEmpty(), "Список подзадач должен быть пустым");
     }
+
     @Test
     void shouldSaveMultipleTasks() throws IOException {
         File testFile = File.createTempFile("multiSave", ".csv");
         FileBackedTaskManager manager = new FileBackedTaskManager(testFile);
         manager.addTask(new Task("Task 1", "Description 1", TaskStatus.NEW));
-        manager.addTask(new Epic("Epic 1", "Epic Description"));
-        manager.addTask(new Subtask("Subtask 1", "Sub Desc", TaskStatus.IN_PROGRESS, 2));
+        manager.addTask(new Task("Task 2", "Description 2", TaskStatus.IN_PROGRESS));
+        manager.addTask(new Task("Task 3", "Description 3", TaskStatus.DONE));
+
         List<String> lines = Files.readAllLines(testFile.toPath());
         assertEquals(5, lines.size(), "Неверное количество строк в файле");
         assertTrue(lines.get(1).startsWith("1,TASK"), "Неверный формат задачи");
-        assertTrue(lines.get(2).startsWith("2,EPIC"), "Неверный формат эпика");
-        assertTrue(lines.get(3).contains("3,SUBTASK"), "Неверный формат подзадачи");
-        assertTrue(lines.get(3).endsWith("2"), "Неверный epicId у подзадачи");
+        assertTrue(lines.get(2).startsWith("2,TASK"), "Неверный формат эпика");
+        assertTrue(lines.get(3).contains("3,TASK"), "Неверный формат подзадачи");
     }
-
 
 
 }
