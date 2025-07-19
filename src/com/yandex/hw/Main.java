e0f6a4009cad92cab2458d97d087e7e0fc9bdea4
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-07-19 10:02:35 +0600

    добавил новый абстрактный класс TaskManagerTest

--- src/com/yandex/hw/Main.java
+++ src/com/yandex/hw/Main.java
@@ -1,9 +1,24 @@
 package com.yandex.hw;
 
 
+import com.yandex.hw.manager.tasks.InMemoryTaskManager;
+import com.yandex.hw.manager.tasks.TaskManager;
+import com.yandex.hw.model.Epic;
+
 public class Main {
 
     public static void main(String[] args) {
+        TaskManager taskManager = new InMemoryTaskManager();
+        taskManager.addTask(new Epic("Первая задача", "Описание"));
+        taskManager.addTask((new Epic("Новая обычная задача", "Описание")));
+        taskManager.getTaskById(1);
+        taskManager.getTaskById(1);
+        taskManager.getTaskById(2);
+        taskManager.getTaskById(2);
+        System.out.println(taskManager.getAllTask(Epic.class));
+        int numberOfTasks = taskManager.getAllTask(Epic.class).size();
+        int actualSizeOfArray = taskManager.getHistory().size();
+        System.out.println( taskManager.getHistory());
 
     }
 }
