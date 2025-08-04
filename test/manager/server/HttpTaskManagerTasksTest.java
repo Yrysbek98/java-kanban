9a6c750a15d2a923400b73e6e1d2128b1b0311d0
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-08-04 22:11:16 +0600

    испрвил ошибку тест testDeleteTask

--- test/manager/server/HttpTaskManagerTasksTest.java
+++ test/manager/server/HttpTaskManagerTasksTest.java
@@ -1,7 +1,7 @@
 package manager.server;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 import com.yandex.hw.manager.tasks.InMemoryTaskManager;
 import com.yandex.hw.manager.tasks.TaskManager;
 import com.yandex.hw.model.Task;
@@ -12,19 +12,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HttpTaskManagerTasksTest {
 
-    // создаём экземпляр InMemoryTaskManager
     TaskManager manager = new InMemoryTaskManager();
-    // передаём его в качестве аргумента в конструктор HttpTaskServer
     HttpTaskServer taskServer = new HttpTaskServer(manager);
     Gson gson = HttpTaskServer.getGson();
 
@@ -82,26 +82,42 @@ public class HttpTaskManagerTasksTest {
 
         HttpResponse<String> postResponse = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
         assertEquals(201, postResponse.statusCode(), "Не удалось создать задачу");
+        HttpRequest getAllRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/tasks"))
+                .GET()
+                .build();
+
+        HttpResponse<String> getAllResponse = client.send(getAllRequest, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, getAllResponse.statusCode(), "Не удалось получить список задач");
+
+        Type taskListType = new TypeToken<List<Task>>() {
+        }.getType();
+        List<Task> tasks = gson.fromJson(getAllResponse.body(), taskListType);
+
+        Optional<Task> matchingTask = tasks.stream()
+                .filter(t -> t.getName().equals(task.getName()) && t.getDescription().equals(task.getDescription()))
+                .findFirst();
+
+        assertTrue(matchingTask.isPresent(), "Созданная задача не найдена");
+        int taskId = matchingTask.get().getId();
 
         HttpRequest deleteRequest = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/tasks/" + 2))
+                .uri(URI.create("http://localhost:8080/tasks/" + taskId))
                 .DELETE()
                 .build();
 
         HttpResponse<String> deleteResponse = client.send(deleteRequest, HttpResponse.BodyHandlers.ofString());
         assertEquals(200, deleteResponse.statusCode(), "Не удалось удалить задачу");
 
-
         HttpRequest getRequest = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/tasks/" + 2))
+                .uri(URI.create("http://localhost:8080/tasks/" + taskId))
                 .GET()
                 .build();
 
         HttpResponse<String> getResponse = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
         assertEquals(404, getResponse.statusCode(), "Задача все еще доступна");
 
-
-        assertNull(manager.getTaskById(1), "Задача найдена в менеджере после удаления");
+        assertNull(manager.getTaskById(taskId), "Задача найдена в менеджере после удаления");
     }
 
 }
